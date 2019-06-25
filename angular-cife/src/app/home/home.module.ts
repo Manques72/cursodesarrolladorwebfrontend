f7817d10d4cc706fd9f4a036202562fca7ef5603
Manques72 <manquess@hotmail.com>
2019-06-25 09:25:20 +0200

    25-06-19

--- angular-cife/src/app/home/home.module.ts
+++ angular-cife/src/app/home/home.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from '@angular/forms'
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { SaludarComponent } from './saludar/saludar.component';
 
 @NgModule({
-  declarations: [HomeComponent, SaludarComponent],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
-    FormsModule,
-    HomeRoutingModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
